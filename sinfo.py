5ad0748a61bafc413f32d0ef489b499245e1aa35
dhrubajyoti16 <dhruva_jyoti@hotmail.com>
2025-07-28 14:03:13 +0530

    Update sinfo.py

--- sinfo.py
+++ sinfo.py
@@ -1,5 +1,5 @@
 import pandas as pd
-a,b,c=int(input (Please enter class, Section and Roll no))
+a,b,c=int(input(Please enter class, Section and Roll no))
 if a==9 and b==3 and c==34:
 	print("You are in")
 elif a==9 and b==3 and c==143:
